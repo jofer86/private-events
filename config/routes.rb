83e0378e1006e45e60fdc6f1f73b9d34c82060f1
jofer86 <jofer86@gmail.com>
2019-06-19 16:18:25 -0500

    created navbar all links working

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   post '/login' , to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
   get '/signup' , to: "users#new"
+  root 'sessions#new'
   resources :users, :only => [ :create, :show]
 
   resources :events, only: [:new,:create,:show,:index]
